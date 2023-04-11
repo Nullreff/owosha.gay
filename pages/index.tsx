96e4596d2a12d3916867e2c3dd2588f5340255ef
Nullreff <github@nullreff.net>
2023-04-10 22:39:45 -0700

    Update packages and fix display not working under dark reader

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.bgWrap}>
+        <div className={styles.owosha}>
           <Image
             className={styles.safetyBadge}
             src={safetyBadge}
